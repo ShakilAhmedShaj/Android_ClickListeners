de3ff1aedb18ef816b9cf212a54418aeb34892ca
SHAJ <shakilahmedshaj@gmail.com>
2018-08-19 03:13:58 -0700

    Click Listener Type 1 Added

--- app/src/main/java/com/t3ch/shaj/clicklisteners/MainActivity.java
+++ app/src/main/java/com/t3ch/shaj/clicklisteners/MainActivity.java
@@ -2,12 +2,30 @@ package com.t3ch.shaj.clicklisteners;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private TextView loginTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.LoginButtonID);
+        loginTextView = findViewById(R.id.TextViewID);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginTextView.setText(R.string.Button_PressMessage);
+            }
+        });
     }
 }
